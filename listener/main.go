c2280b4678040f7bd833d17a27cacb8f7eeb7820
Blackmorse <blackmorse@live.com>
2024-11-23 13:26:14 +0400

    Fix

--- listener/main.go
+++ listener/main.go
@@ -17,6 +17,18 @@ import (
 	"tondexer/stonfiv2"
 )
 
+type Config struct {
+	ConsoleToken      string   `yaml:"console_token" env:"CONSOLE_TOKEN" env-default:""`
+	DbHost            string   `yaml:"db_host" env:"DB_HOST" env-default:"localhost"`
+	DbPort            uint     `yaml:"db_port" env:"DB_PORT" env-default:"9000"`
+	DbUser            string   `yaml:"db_user" env:"DB_USER" env-default:"default"`
+	DbPassword        string   `yaml:"db_password" env:"DB_PASSWORD" env-default:""`
+	DbName            string   `yaml:"db_name" env:"DB_NAME" env-default:"default"`
+	StonfiV1Addresses []string `yaml:"stonfiv1_addresses" env:"STONFIV1_ADDRESSES" env-default:""`
+	StonfiV2Addresses []string `yaml:"stonfiv2_addresses" env:"STONFIV2_ADDRESSES" env-default:""`
+	DedustAddresses   []string `yaml:"dedust_addresses" env:"DEDUST_ADDRESSES" env-default:""`
+}
+
 func subscribeToAccounts(streamingApi *tonapi.StreamingAPI, accounts []string, incomingTransactionsChannel chan string) {
 	for {
 		e := streamingApi.WebsocketHandleRequests(context.Background(), func(ws tonapi.Websocket) error {
--- web/main.go
+++ web/main.go
@@ -15,6 +15,14 @@ type DexPeriodRequest struct {
 	Dex    string `form:"dex" binding:"omitempty,oneof=all stonfi dedust"`
 }
 
+type Config struct {
+	DbHost     string `yaml:"db_host" env:"DB_HOST" env-default:"localhost"`
+	DbPort     uint   `yaml:"db_port" env:"DB_PORT" env-default:"9000"`
+	DbUser     string `yaml:"db_user" env:"DB_USER" env-default:"default"`
+	DbPassword string `yaml:"db_password" env:"DB_PASSWORD" env-default:""`
+	DbName     string `yaml:"db_name" env:"DB_NAME" env-default:"default"`
+}
+
 func main() {
 	var cfg Config
 
